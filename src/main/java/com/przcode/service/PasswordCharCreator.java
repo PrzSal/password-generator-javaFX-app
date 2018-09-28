a1f98a48aa7dd9bc67312a0052c058e5dfc4ec4e
Przemo8922 <przemyslawsalak@gmail.com>
2018-09-29 00:40:54 +0200

    remove field isMixLowerAndUpperCase

--- src/main/java/com/przcode/service/PasswordCharCreator.java
+++ src/main/java/com/przcode/service/PasswordCharCreator.java
@@ -4,29 +4,25 @@ public enum PasswordCharCreator {
     isLowerCasetrue {
         @Override
         public String addChars() {
-            return new String("abcdefghijklmnopqrstuwxyz");
+            return "abcdefghijklmnopqrstuwxyz";
         }
 
     }, isUpperCasetrue {
         @Override
         public String addChars() {
-            return new String("ABCDEFGHIJKLMNOPQRSTUWXYZ");
+            return "ABCDEFGHIJKLMNOPQRSTUWXYZ";
         }
 
-    },isMixLowerAndUpperCasetrue {
-        @Override
-        public String addChars() {
-            return new String("abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ");
-        }
     }, isSpecialCharactertrue {
         @Override
         public String addChars() {
-            return new String("!@#$%^&*/\\");
+            return "!@#$%^&*/\\";
         }
+
     }, isDigittrue {
         @Override
         public String addChars() {
-            return new String("1234567890");
+            return "1234567890";
         }
     };
     public abstract String addChars();
