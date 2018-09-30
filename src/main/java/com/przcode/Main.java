0102d0f4cbd6f5b26fee7526fc307b0703b86ca8
Przemo8922 <przemyslawsalak@gmail.com>
2018-10-01 00:59:37 +0200

    add style.css to Scene

--- src/main/java/com/przcode/Main.java
+++ src/main/java/com/przcode/Main.java
@@ -16,8 +16,12 @@ public class Main extends Application
         FXMLLoader sampleLoader = new FXMLLoader(getClass().getResource("/content.fxml"));
         root.setCenter(sampleLoader.load());
         primaryStage.setTitle("Password Generator");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        Scene scene = new Scene(root, 550, 400);
+        primaryStage.setScene(scene);
+        scene.getStylesheets().add("/style.css");
         primaryStage.show();
+        primaryStage.setMinWidth(primaryStage.getWidth());
+        primaryStage.setMinHeight(primaryStage.getHeight());
     }
     public static void main( String[] args )
     {
