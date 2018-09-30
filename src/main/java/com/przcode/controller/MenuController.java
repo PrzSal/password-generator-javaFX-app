fa6b438c37373586b4f499cef3e1bddd45c1d299
Przemo8922 <przemyslawsalak@gmail.com>
2018-10-01 00:53:07 +0200

    add method saveFile().

--- src/main/java/com/przcode/controller/MenuController.java
+++ src/main/java/com/przcode/controller/MenuController.java
@@ -1,4 +1,34 @@
 package com.przcode.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+import javafx.stage.FileChooser;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MenuController {
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    public void saveFile() {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilter =
+                new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+        File file = fileChooser.showSaveDialog(menuBar.getScene().getWindow());
+
+        if (file != null) {
+            FileWriter fileWriter;
+            try {
+                fileWriter = new FileWriter(file);
+                fileWriter.write(ContentController.contentController.passwordField.getText());
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
